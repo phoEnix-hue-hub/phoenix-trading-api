8d83a1675084463230dc4eb2cc9bd8b45c9aead1
phoEnix-hue-hub <your-phoEnix-hue-hub-emalbecky593@gmail.com>
2025-06-28 18:28:39 +0100

    Add typing to middleware in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { Request, Response, NextFunction } from 'express';
 
 @Module({
   imports: [AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // Example: Skip authentication for register route
-    consumer.apply((req, res, next) => {
-      if (req.path === '/api/auth/register' && req.method === 'POST') return next();
-      // Apply your guard here for other routes
-    }).forRoutes('*');
+    consumer
+      .apply((req: Request, res: Response, next: NextFunction) => {
+        if (req.path === '/api/auth/register' && req.method === 'POST') return next();
+        // Apply your guard here for other routes if needed
+      })
+      .forRoutes('*');
   }
 }
